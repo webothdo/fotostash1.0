2fcac668e15811dd9d02d06759d61cda4d366a90
webothdo <joshuaogboloo@gmail.com>
2024-05-19 17:39:31 +0100

    testing

--- server/api/accept-photo.post.js
+++ server/api/accept-photo.post.js
@@ -2,10 +2,12 @@ import { approvedPhoto } from "../database/schema";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-
+  return body;
   try {
     await db.insert(approvedPhoto).values({
       id: body.id,
     });
-  } catch (error) {}
+  } catch (error) {
+    return error;
+  }
 });
